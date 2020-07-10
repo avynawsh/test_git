47a11015b32812d53a655e3b4e8b71f1af1ecdd6
Avinash Reddy <avinashreddy@Avinashs-MacBook-Pro.local>
2020-07-10 23:16:57 +0530

    Initial Commit

---
+++ test.py
@@ -0,0 +1,17 @@
+import sys
+
+print(sys.version)
+print(sys.executable)
+
+import numpy as np
+
+b = np.zeros(3)
+print(b)
+
+print("Hello")
+a = np.ones(4)
+
+print(a)
+
+name = input('WHat is your name?\n')
+print(name)a
